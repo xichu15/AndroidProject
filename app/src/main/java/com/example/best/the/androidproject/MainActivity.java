36db1f0dacc0baa0d4ec93cb249f17c2434203b2
xichu15 <xichu15@gmail.com>
2016-12-01 20:07:02 +0100

    Second commit so far

---
+++ app/src/main/java/com/example/best/the/androidproject/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.best.the.androidproject;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+//SNIKI COMMIT
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+}
