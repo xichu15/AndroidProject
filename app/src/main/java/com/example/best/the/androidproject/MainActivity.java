8196451e70e4d1251e124c152c0994bb932a5f4a
xichu15 <xichu15@gmail.com>
2016-12-05 00:55:16 +0100

    StartService - notificationService, TaskList

--- app/src/main/java/com/example/best/the/androidproject/MainActivity.java
+++ app/src/main/java/com/example/best/the/androidproject/MainActivity.java
@@ -6,13 +6,23 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+
 public class MainActivity extends AppCompatActivity {
 //SNIKI COMMIT
     Button baton;
+    TaskList taskList = new TaskList();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        createTaskList();
+
+        startService(new Intent(getApplicationContext(), NotificationService.class));
 
         //TU BYLAM ~Dudzilla
         baton = (Button) findViewById(R.id.settingsBtn);
@@ -26,6 +36,27 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
+    private void createTaskList(){
+        Date d1 = new Date();
+        Calendar cal1 = Calendar.getInstance();
+        cal1.set(2016, 11, 4, 18, 40);
+
+        /*Date d1 = new Date("12/4/2016 18:40:00");
+        Calendar cal1 = Calendar.getInstance();
+        cal1.setTime(d1);*/
+
+        d1 = new Date("12/4/2016 19:25:00");
+        Calendar cal2 = Calendar.getInstance();
+        cal2.setTime(d1);
+
+        d1 = new Date("12/4/2016 18:10:00");
+        Calendar cal3 = Calendar.getInstance();
+        cal3.setTime(d1);
+
+        taskList.addTask(new TestTask("First", cal1));
+        taskList.addTask(new TestTask("Second", cal2));
+        taskList.addTask(new TestTask("Third", cal3));
+    }
 
 
 
