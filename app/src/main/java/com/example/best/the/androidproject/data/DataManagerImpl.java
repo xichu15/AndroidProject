94d2b4d0bdf03d73607108d5cdb2570e00b7b78d
Michal Mizera-3 <MotorolaScrumWorkshop14@gmail.com>
2016-12-12 13:38:23 +0100

    final part of Data manager

--- app/src/main/java/com/example/best/the/androidproject/data/DataManagerImpl.java
+++ app/src/main/java/com/example/best/the/androidproject/data/DataManagerImpl.java
@@ -32,8 +32,8 @@ public class DataManagerImpl implements DataManager {
     public DataManagerImpl(Context context) {
         this.context = context;
 
-        SQLiteOpenHelper openHelper = new ExampleOpenHelper(this.context);
-        db = openHelper.getWritableDatabase();
+        //SQLiteOpenHelper openHelper = new ExampleOpenHelper(this.context);
+        // db = openHelper.getWritableDatabase();
 
         taskDao = new TaskDao(db);
         taskPeriodicityDao = new TaskPeriodicityDao(db);
@@ -50,7 +50,7 @@ public class DataManagerImpl implements DataManager {
     @Override
     public List<Task> getAllTasks() {
         List<Task> tasks = taskDao.getAll();
-        return task;
+        return tasks;
     }
 
     @Override
@@ -61,105 +61,141 @@ public class DataManagerImpl implements DataManager {
 
     @Override
     public long saveTask(Task task) {
-        return 0;
+        long taskId = 0;
+
+        try {
+            db.beginTransaction();
+            taskId = taskDao.save(task);
+            db.setTransactionSuccessful();
+            } catch(SQLException e) {
+                Log.e("save task error", "Blad przy zapisie zadania", e);
+                taskId = 0;
+            } finally {
+                db.endTransaction();
+            }
+            return taskId;
     }
 
     @Override
     public boolean updateTask(Task task, long taskId) {
-        return false;
+        saveTask(task);
+        deleteTask(getTask(taskId));
+        return true;
     }
 
     @Override
     public void deleteTask(Task task) {
-
+        try {
+            db.beginTransaction();
+            taskDao.delete(task);
+            db.setTransactionSuccessful();
+        } catch (SQLException e) {
+            Log.e("delete task error", "Błąd przy usuwaniu zadania", e);
+        } finally {
+            db.endTransaction();
+        }
     }
 
     @Override
     public TaskPeriodicity getTaskPeriodicity(long taskPeriodicityId) {
-        return null;
+        return taskPeriodicityDao.get(taskPeriodicityId);
     }
 
     @Override
     public List<TaskPeriodicity> getAllTaskPeriodicty() {
-        return null;
+        return taskPeriodicityDao.getAll();
     }
 
     @Override
     public TaskPeriodicity findTaskPeriodicity(String name) {
-        return null;
+        TaskPeriodicity taskPeriodicity = taskPeriodicityDao.find(name);
+        return taskPeriodicity;
     }
 
     @Override
     public long saveTaskPeriodicity(TaskPeriodicity taskPeriodicity) {
-        return 0;
+        long taskPeriodicityId = 0;
+        taskPeriodicityId = taskPeriodicityDao.save(taskPeriodicity);
+        return taskPeriodicityId;
     }
 
     @Override
     public boolean updateTaskPeriodicity(TaskPeriodicity taskPeriodicity, long taskPeriodicityId) {
-        return false;
+        saveTaskPeriodicity(taskPeriodicity);
+        deleteTaskPeriodicity(taskPeriodicityDao.get(taskPeriodicityId));
+        return true;
     }
 
     @Override
     public void deleteTaskPeriodicity(TaskPeriodicity taskPeriodicity) {
-
+        taskPeriodicityDao.delete(taskPeriodicity);
     }
 
     @Override
     public TaskType getTaskType(long taskTypeId) {
-        return null;
+        return taskTypeDao.get(taskTypeId);
     }
 
     @Override
     public List<TaskType> getAllTaskType() {
-        return null;
+        return taskTypeDao.getAll();
     }
 
     @Override
     public TaskType findTaskType(String name) {
-        return null;
+        return taskTypeDao.find(name);
     }
 
     @Override
     public long saveTaskType(TaskType taskType) {
-        return 0;
+        long taskTypeId = 0;
+        taskTypeId = taskTypeDao.save(taskType);
+        return taskTypeId;
     }
 
     @Override
     public boolean updateTaskType(TaskType taskType, long taskTypeId) {
-        return false;
+        saveTaskType(taskType);
+        deleteTaskPeriodicity(taskPeriodicityDao.get(taskTypeId));
+        return true;
     }
 
     @Override
     public void deleteTaskType(TaskType taskType) {
-
+        taskTypeDao.delete(taskType);
     }
 
     @Override
     public TaskPriority getTaskPriority(long taskPriorityId) {
+        return taskPriorityDao.get(taskPriorityId);
     }
 
     @Override
     public List<TaskPriority> getAllTaskPriority() {
-        return null;
+        return taskPriorityDao.getAll();
     }
 
     @Override
     public TaskPriority findTaskPriority(String name) {
-        return null;
+        return taskPriorityDao.find(name);
     }
 
     @Override
     public long saveTaskPriority(TaskPriority taskPriority) {
-        return 0;
+        long taskPriorityId = 0;
+        taskPriorityDao.save(taskPriority);
+        return taskPriorityId;
     }
 
     @Override
     public boolean updateTaskPriority(TaskPriority taskPriority, long taskPriorityId) {
-        return false;
+        saveTaskPriority(taskPriority);
+        deleteTaskPriority(taskPriorityDao.get(taskPriorityId));
+        return true;
     }
 
     @Override
     public void deleteTaskPriority(TaskPriority taskPriority) {
-
+        taskPriorityDao.delete(taskPriority);
     }
 }
